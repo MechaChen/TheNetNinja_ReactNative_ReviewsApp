04699d551edc488807c3c42b83d5d5cb284ce89a
Benson <tumlivein@gmail.com>
2019-12-19 00:02:50 +0800

    18. Global Styles

---
+++ styles/global.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        padding: 24,
+    },
+    titleText: {
+        fontFamily: 'nunito-bold',
+        fontSize: 18,
+        color: '#333'
+    },
+    paragraph: {
+        marginVertical: 8,
+        lineHeight: 20,
+    }
+});
