a71707a7a26997d0c45412270866328a3b86cee9
Benson <tumlivein@gmail.com>
2019-12-19 22:50:49 +0800

    19. 20. Stack Navigator

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import * as Font from 'expo-font';
 import Home from './screens/home';
 import { AppLoading } from 'expo';
+import Navigator from './routes/homeStack';
 
 const getFonts = () => Font.loadAsync({
     'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
@@ -13,7 +14,7 @@ export default function App() {
 
     if (fontsLoaded) {
         return (
-            <Home />
+            <Navigator />
         );
     } else {
         return (
