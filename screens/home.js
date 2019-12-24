4986b70ee6585428bf5d67a03795d0c5120f574c
Benson Chen <tumlivein@gmail.com>
2019-12-24 09:50:22 +0800

    21. Navigating Around

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({ navigation }) {
+
+    const pressHandler = () => {
+        navigation.navigate('ReviewDetails');
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button
+                title="go to review dets"
+                onPress={pressHandler}
+            />
         </View>
     );
 }
