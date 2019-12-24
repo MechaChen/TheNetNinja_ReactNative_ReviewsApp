89d38c14611ec48839c1c577e4d316a1e2b37db2
Benson Chen <tumlivein@gmail.com>
2019-12-24 10:30:50 +0800

    23. Navigation Options

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -5,13 +5,24 @@ import ReviewDetails from '../screens/reviewDetails';
 
 const screens = {
     Home: {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: 'GameZone',
+        },
     },
     ReviewDetails: {
-        screen: ReviewDetails
+        screen: ReviewDetails,
+        navigationOptions: {
+            title: 'Review Details',
+        },
     }
 }
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor: '#444',
+        headerStyle: { backgroundColor: '#eee', height: 60 },
+    },
+});
 
 export default createAppContainer(HomeStack);
