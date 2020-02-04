e352c04f5a0d91e96a63714e4a9cf68dce89da07
Benson Chen <tumlivein@gmail.com>
2020-02-04 09:38:04 +0800

    32. Validation with Yup

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -10,7 +10,7 @@ const screens = {
         screen: Home,
         navigationOptions: ({ navigation }) => {
             return {
-                headerTitle: () => <Header navigation={navigation} title="Gamezone" />,
+                headerTitle: () => <Header navigation={navigation} title="GameZone" />,
             }
         },
     },
